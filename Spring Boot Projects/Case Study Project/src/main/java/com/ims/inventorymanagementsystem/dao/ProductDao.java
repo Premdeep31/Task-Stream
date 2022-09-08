59a9f028e020920811e24c3c9d965cce272aae20
Premdeep31 <110416880+Premdeep31@users.noreply.github.com>
2022-09-09 01:56:00 +0530

    Delete Spring Boot Projects/Case Study Project/src directory

--- Spring Boot Projects/Case Study Project/src/main/java/com/ims/inventorymanagementsystem/dao/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package com.ims.inventorymanagementsystem.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ims.inventorymanagementsystem.entity.Product;
-
-public interface ProductDao extends JpaRepository<Product, Integer> {
-	
-}
